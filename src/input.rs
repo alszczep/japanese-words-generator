b2885ed0d774ed674d6f0faefebfbb8a714b403e
alszczep <al.szczepura@gmail.com>
2023-01-26 20:36:48 +0100

    Add choosing syllabary

---
+++ src/input.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+pub enum Input {
+    Hirgana = 0,
+    Katakana = 1,
+}
+
+pub fn ask_for_input() -> Input {
+    println!("Syllabary to generate with:");
+    println!("   [1] Hiragana");
+    println!("   [2] Katakana");
+    
+    let mut input = String::new();
+    while input != "1" && input != "2" {
+        let result = io::stdin().read_line(&mut input);
+        match result {
+            Ok(_) => {
+                if input != "1" && input != "2" {
+                    break;
+                }
+            },
+            Err(_) => {},
+        }
+        
+        println!("Incorrect input")
+    }
+    
+    if input == "1" {
+        Input::Hirgana
+    } else {
+        Input::Katakana
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use crate::kana::Syllabary;
+
 pub mod kana;
 pub mod config;
+pub mod input;
 
 fn main() {
-    dbg!(kana::load_katakana());
     let cfg = config::load_config();
-    dbg!(cfg);
+    let syllabary_input = input::ask_for_input();
+    let mut syllabary: Syllabary;
+    match syllabary_input {
+        input::Input::Hirgana => syllabary = kana::load_hiragana(),
+        input::Input::Katakana => syllabary = kana::load_katakana(),
+    }
 }
